29e90c013464cc8dbb8314a6be47592fc28d4f2b
Vibhu <vibhu98mishra@gmail.com>
2021-06-17 01:48:34 +0530

    authenication routes

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(`mongodb+srv://shop-rest-api:${process.env.MONGO_ATLAS_PW}@cluster0.sewyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true
 });
 
 app.use(morgan('dev'));
@@ -32,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
