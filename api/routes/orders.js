f35a0e7d2412aaca3f41f951e8772d0c28a5def8
Vibhu <vibhu98mishra@gmail.com>
2021-06-17 02:18:27 +0530

    protected route with token

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 const Order = require('../models/orders');
 const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find()
     .select('product quantity _id')
     .populate('product', 'name')
@@ -27,7 +28,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
 
     Product.findById(req.body.productId)
     .then(
@@ -62,7 +63,7 @@ router.post('/', (req, res, next) => {
 
 })
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId)
     .populate('product')
     .exec()
@@ -81,7 +82,7 @@ router.get('/:orderId', (req, res, next) => {
     })
 })
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({_id: req.params.orderId})
     .exec()
     .then(result => {
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -63,7 +64,7 @@ router.get('/', (req, res, next) => {
     })
 })
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
 
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -73,7 +74,6 @@ router.post('/', upload.single('productImage'), (req, res, next) => {
     })
 
     product.save().then(result => {
-        console.log(res);
         res.status(201).json({
             message: `Created Product sucessfully`,
             createdProduct: {
@@ -113,7 +113,7 @@ router.get('/:productId', (req, res, next) => {
     });
 })
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
 
     const updateOps = {};
@@ -136,7 +136,7 @@ router.patch('/:productId', (req, res, next) => {
     }) 
 })
 
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.remove({_id: id}).exec()
     .then(result => {
